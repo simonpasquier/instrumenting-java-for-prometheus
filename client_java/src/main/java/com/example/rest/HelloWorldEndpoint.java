204743289d91e62528258620eca3e835f125b777
Simon Pasquier <spasquie@redhat.com>
2018-12-04 16:55:55 +0100

    move to subdirectory

--- client_java/src/main/java/com/example/rest/HelloWorldEndpoint.java
+++ client_java/src/main/java/com/example/rest/HelloWorldEndpoint.java
@@ -1,5 +1,6 @@
 package com.example.rest;
 
+import io.prometheus.client.Counter;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
@@ -7,10 +8,14 @@ import javax.ws.rs.Produces;
 
 @Path("/hello")
 public class HelloWorldEndpoint {
+  static final Counter requests = Counter.build()
+    .name("rest_requests_total").help("Total requests.")
+    .labelNames("method","handler").register();
 
   @GET
   @Produces("text/plain")
   public Response doGet() {
+    requests.labels("GET","/hello").inc();
     return Response.ok("Hello from Thorntail!").build();
   }
 }
